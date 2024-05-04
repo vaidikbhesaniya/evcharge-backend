ca17683454e56e518ff686f601e295b29b1eda0e
Mr. V <vaidikbhesaniya1212@gmail.com>
2024-05-04 18:25:26 +0530

    init

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(
     cors({
-        origin: "*",
+        origin: "https://evcharge-theta.vercel.app",
         methods: ["GET", "POST", "PUT", "DELETE"],
         credentials: true,
     })

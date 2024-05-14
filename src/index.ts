d904b1867ec4d429b0ee155197cdeee748df248f
Mr. V <vaidikbhesaniya1212@gmail.com>
2024-05-14 16:51:23 +0530

    getstation

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import "dotenv/config";
 import express, { Request, Response } from "express";
 import { router } from "./routes/router.js";
 
-// import prisma from "./db/index.js";
+import prisma from "./db/index.js";
 // import station_data from "./controllers/export.js";
 
 const app = express();
@@ -23,17 +23,42 @@ app.use(
 
 // async function seedData() {
 //     const gasStations = station_data;
+//     const batchsize = 2000;
+//     const tatoal_tupples = 80000;
 
 //     try {
-//         await prisma.station.createMany({
-//             data: gasStations.map((station) => ({
-//                 stationName: station.Station_Name,
-//                 stationAddress: station.Station_address,
-//                 latitude: parseFloat(station.Latitude),
-//                 longitude: parseFloat(station.Longitude),
-//                 category: "Ev",
-//             })),
-//         });
+//         for (let i = 0; i < tatoal_tupples; i += batchsize) {
+//             const insertedtupple = [];
+
+//             for (let j = 0; j < batchsize; j++) {
+//                 const tupple = {
+//                     stationName: station_data[j].Station_Name,
+//                     stationAddress: station_data[j].Station_address,
+//                     latitude: parseFloat(station_data[j].Latitude),
+//                     longitude: parseFloat(station_data[j].Longitude),
+//                     category: "Ev",
+//                     rating: "",
+//                     state: station_data[j]?.State || "",
+//                     city: station_data[j]?.City || "",
+//                     country: station_data[j]?.Country || "",
+//                     stationphone: station_data[j].Station_Phone,
+//                     cardaccepted: station_data[j].Cards_Accepted || "",
+//                     EVConnectorTypes: station_data[j].EV_Connector_Types,
+//                     opendate: station_data[j].Open_Date,
+//                     zipcode: station_data[j].ZIP,
+//                     website: station_data[j].EV_Network_Web,
+//                     openTime: station_data[j].Access_Days_Time,
+//                 };
+
+//                 insertedtupple.push(tupple);
+//             }
+
+//             await prisma.station.createMany({
+//                 data: insertedtupple,
+//                 skipDuplicates: true,
+//             });
+//         }
+
 //         console.log("Data seeded successfully.");
 //     } catch (error) {
 //         console.error("Error seeding data:", error);
@@ -43,7 +68,7 @@ app.use(
 // }
 
 // async function seedData() {
-//     const gasStations = station_data;
+//     // const gasStations = station_data;
 
 //     try {
 //         // await prisma.station.createMany({
@@ -66,6 +91,43 @@ app.use(
 
 // seedData();
 
+// async function retrieveDataInChunks() {
+//     let offset = 0;
+//     const batchSize = 1000; // Adjust batch size as per your requirement
+
+//     while (true) {
+//         const chunk = await prisma.station.findMany({
+//             skip: offset,
+//             take: batchSize,
+//         });
+
+//         // Process the chunk of data here
+
+//         if (chunk.length < batchSize) {
+//             break; // Exit loop if the last chunk is retrieved
+//         }
+
+//         offset += batchSize;
+//     }
+// }
+
+// retrieveDataInChunks();
+app.get("/station/:offset", async (req, res) => {
+    try {
+        let offset = parseInt(req.params.offset, 10);
+        const batchSize = 1000; // Adjust batch size as per your requirement
+
+        const chunk = await prisma.station.findMany({
+            skip: offset,
+            take: batchSize,
+        });
+
+        // Process the chunk of data here
+        res.status(200).json(chunk);
+    } catch (error) {
+        console.log(error);
+    }
+});
 //Routes
 app.use("/api/v1", router);
 app.use("/api/v1/health", (req: Request, res: Response) => {

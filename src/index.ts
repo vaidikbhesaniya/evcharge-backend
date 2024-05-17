a51c392a36dea78341516872d4b8012846cbe3ab
Mr. V <vaidikbhesaniya1212@gmail.com>
2024-05-18 00:10:51 +0530

    stationdetail

--- src/index.ts
+++ src/index.ts
@@ -15,7 +15,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(
     cors({
-        origin: ["https://evcharge-omega.vercel.app", "http://localhost:5173"],
+        origin: ["https://evpointer.vercel.app/", "http://localhost:5173"],
         methods: ["GET", "POST", "PUT", "DELETE"],
         credentials: true,
     })
